172d67e39a003e35b0d3fb50972785c430f75dcb
JaeHoon <87574833+tlshoon@users.noreply.github.com>
2022-09-17 22:33:46 +0900

    Add files via upload

--- binary search.py
+++ binary search.py
@@ -13,3 +13,53 @@
 # array = input().split()
 #
 # print(sequential_search(n,target,array))
+
+# 재귀 함수로 구현한 이진 탐색
+# def binary_search(array, target, start, end):
+#     if start > end:
+#         return None
+#     mid = (start+end) // 2 # 찾은 경우 중간점 인덱스 반환
+#     if array[mid] == target:
+#         return mid
+#     elif array[mid] > target:
+#         return binary_search(array, target, start, mid-1)
+#     else:
+#         return binary_search(array, target, mid + 1, end)
+#
+# n, target = list(map(int,input().split()))
+# array = list(map(int,input().split()))
+#
+# result = binary_search(array,target,0,n-1)
+#
+# if result == None:
+#     print("원소가 존재하지 않습니다.")
+# else:
+#     print(result+1)
+
+# 반복문으로 구현한 이진 탐색
+# def binary_search(array,target,start,end):
+#     while start <= end:
+#         mid = (start+end) // 2
+#         if array[mid] == target:
+#             return mid
+#         elif array[mid] > target:
+#             end = mid - 1
+#         else:
+#             start = mid + 1
+#     return None
+#
+# n, target = list(map(int,input().split()))
+# array = list(map(int,input().split()))
+#
+# result = binary_search(array,target,0,n-1)
+#
+# if result == None:
+#     print("원소가 존재하지 않습니다.")
+# else:
+#     print(result+1)
+
+# 한 줄 입력받아 출력하는 소스코드
+# import sys
+# input_data = sys.stdin.readline().rstrip()
+#
+# print(input_data)
